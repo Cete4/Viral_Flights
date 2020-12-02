c16e2f1e8aa757a2f9e7bcc576c8d28b9e0173da
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-02 13:18:21 -0600

    Added nav with nav items

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a className="navbar-brand" href="/">Viral Flights</a>
+            <a className="navbar-brand" href="/">Flights</a>
+            <a className="navbar-brand" href="/">Corona Virus Travel News</a>
+            <a className="navbar-brand" href="/">Blog</a>
+        </nav>
+    );
+}
+
+export default Nav;
