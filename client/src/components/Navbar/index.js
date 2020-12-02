23def769fdb96cf686f128e25c088c7583663a8f
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-02 13:46:21 -0600

    Refactor the navbar for react bootstrap

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
 
 function Nav() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="/">Viral Flights</a>
-            <a className="navbar-brand" href="/Flights">Flights</a>
-            <a className="navbar-brand" href="/CoronaVirusTravelNews">Corona Virus Travel News</a>
-            <a className="navbar-brand" href="/Blog">Blog</a>
-        </nav>
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">Viral Flights</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="Flights">Flights</Nav.Link>
+                    <Nav.Link href="CoronaTravelNews">Corona Travel News</Nav.Link>
+                    <Nav.Link href="Blog">Blog</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search for Flights" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+        </>
     );
 }
 
