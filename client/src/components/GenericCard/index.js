cb27a3efd44a26a76344b3d64e0ba6b3e8fce07e
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-02 13:32:04 -0600

    Right idea wrong files

--- client/src/components/GenericCard/index.js
+++ client/src/components/GenericCard/index.js
@@ -8,11 +8,11 @@ function BookCard(props) {
             {/* TODO: Add images of the destination if possible */}
             {/* <Card.Img variant="left" src={props.src} /> */}
             <Card.Body>
-                <Card.Title>Flight: {props.destination}</Card.Title>
-                <Card.Text id="heavy">Cost: <p>{props.cost}</p></Card.Text>
-                <Card.Text id="heavy">Date: <p>{props.Date}</p></Card.Text>
-                <Card.Text id="heavy">Link: <a href={props.link} target={"_blank"} rel="noopener noreferrer">{props.link}</a></Card.Text>
-                <Card.Text>{props.description}</Card.Text>
+                <Card.Title></Card.Title>
+                <Card.Text></Card.Text>
+                <Card.Text></Card.Text>
+                <Card.Text></Card.Text>
+                <Card.Text></Card.Text>
             </Card.Body>
         </Card>
     )
