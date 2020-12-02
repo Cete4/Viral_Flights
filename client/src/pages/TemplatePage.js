feb22acde8fffdbe53ce2ffda4de61118b7a6175
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-02 14:00:16 -0600

    Added generic things

---
+++ client/src/pages/TemplatePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import BookCard from "../components/BookCard";
+import Search from "../components/Search";
+
+class TemplatePage extends Component {
+
+}
+
+export default TemplatePage;
