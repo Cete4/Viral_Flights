bef6ab0a99a7e140ac7987bb630c38f2bfbb5b25
Charline <charlinerlach@gmail.com>
2020-12-02 23:11:40 -0600

    Login updates - page and component

---
+++ client/src/pages/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Component } from "react";
+// import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import Navbar from "../components/Navbar";
+// import FlightsCard from "../components/FlightsCard";
+import Login from "./containers/Login";
+
+class Login extends Component {
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col size="md-12">
+                            <Navbar />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col size="md-12">
+
+                        </Col>
+                    </Row>
+                </Container >
+            </div >
+        );
+    }
+}
+
+export default Login;
