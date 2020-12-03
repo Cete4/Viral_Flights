bd0f175a5bbe71c20d6d0966652e61738c98ee9e
Raymond Gordon <rjgordon26@gmail.comgit config --global user.email rjgordon26@gmail.com>
2020-12-03 17:39:32 -0600

    updated api key files

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,109 +1,96 @@
-// import React, { Component } from "react";
-// import SearchForm from "./SearchForm";
-// import FlightsCard from "./FlightsCard";
-// import flightAPI from "../utils/flightAPI";
-// import "../styles/Result.css";
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import FlightsCard from "./FlightsCard";
+import flightAPI from "../utils/flightAPI";
 
-// class Container extends Component {
-//     state = {
-//         result: [],
-//         filter: "",
-//         filterBy: "quotes",
-//         currentSort: "default",
-//         sortField: ""
+class Container extends Component {
+    state = {
+        result: [],
+        filter: "",
+        filterBy: "quotes",
+        currentSort: "default",
+        sortField: ""
 
-//     };
+    };
 
-//     componentDidMount() {
-//         flightAPI.search()
-//             .then(res => {
-//                 console.log(res)
-//                 this.setState({
-//                     result: res.data.results.map((e, i) => ({
-//                         quotes: e.quotes.MinPrice,
-//                         departure: e.quotes.OutboundLeg.DepartureDate,
-//                         carriers: e.carriers.Name,
-//                         place: e.places.CityName,
-//                         airport: e.places.Name,
-//                         key: i
-//                     }))
-//                 })
-//                 console.log(quotes)
+    componentDidMount() {
+        flightAPI.search()
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    result: res.data.results.map((e, i) => ({
+                        quotes: e.quotes.MinPrice,
+                        departure: e.quotes.OutboundLeg.DepartureDate,
+                        carriers: e.carriers.Name,
+                        place: e.places.CityName,
+                        airport: e.places.Name,
+                        key: i
+                    }))
+                })
+                console.log(quotes)
 
-//             })
-//             .catch(err => console.log(err));
-//     }
+            })
+            .catch(err => console.log(err));
+    }
 
-//     handleFormSubmit = event => {
-//         event.preventDefault();
-//         const value = event.target.value;
-//         const name = event.target.name;
-//         console.log(name);
-//         console.log(value);
-//         this.setState({
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const value = event.target.value;
+        const name = event.target.name;
+        console.log(name);
+        console.log(value);
+        this.setState({
 
-//             [name]: value
+            [name]: value
 
-//         });
+        });
 
-//     };
+    };
 
-//     handleInputChange = event => {
-//         event.preventDefault();
-//         console.log(event);
-//         const value = event.target.value;
-//         const name = event.target.name;
-//         console.log(value);
-//         console.log(name);
-//         this.setState({
+    handleInputChange = event => {
+        event.preventDefault();
+        console.log(event);
+        const value = event.target.value;
+        const name = event.target.name;
+        console.log(value);
+        console.log(name);
+        this.setState({
 
-//             [name]: value
+            [name]: value
 
-//         });
-//     };
+        });
+    };
 
-//     render() {
-//         return (
-//             <div className="container">
-//                 <div className="row">
-//                 </div>
-//                 <div className="row">
-//                     <div className="col-md-6">
-//                         <SearchForm
-//                             value={this.state.search}
-//                             handleInputChange={this.handleInputChange}
-//                             handleFormSubmit={this.handleFormSubmit}
-//                         />
-//                     </div>
-//                 </div>
+    render() {
+        return (
+            <p>
+                <div>
+                    <SearchForm
+                        value={this.state.search}
+                        handleInputChange={this.handleInputChange}
+                        handleFormSubmit={this.handleFormSubmit}
+                    />
+                </div>
+                <div>
+                    <p>
+                        {[...this.state.result].map((item) =>
+                            <FlightsCard
+                                place={item.place}
+                                airport={item.airport}
+                                quotes={item.quotes}
+                                carriers={item.carriers}
+                                departure={item.departure}
+                                key={item.key}
+                            />
+                        )}
 
-//                 <div className="row">
-//                     <table className="table">
-//                         <p>
-//                             <th scope="col">Photo</th>
-//                             <th>First Name</th>
-//                             <th scope="col">Last Name </th>
-//                             <th scope="col">Email</th>
-//                             <th scope="col">Phone</th>
-//                         </p>
-//                         {[...this.state.result].map((item) =>
-//                             <FlightsCard
-//                                 place={item.place}
-//                                 airport={item.airport}
-//                                 quotes={item.quotes}
-//                                 carriers={item.carriers}
-//                                 departure={item.departure}
-//                                 key={item.key}
-//                             />
-//                         )}
-
-//                     </table>
-//                 </div>
+                    </p>
+                </div>
 
 
-//             </div>
-//         );
-//     }
-// }
+            </p>
+        );
+    }
+}
 
-// export default Container;
+export default Container;
--- client/src/utils/covidAPI.js
+++ client/src/utils/covidAPI.js
@@ -1,12 +1,15 @@
-const axios = require("axios");
 
-axios({
-  "method": "GET",
-  "url": "https://api.covidtracking.com/v1/states/current.json",
-})
-  .then((response) => {
-    console.log(response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+  import axios from "axios";
+
+
+  export default {
+    getCovidInfo: function() {
+      return axios.get("https://api.covidtracking.com/v1/states/current.json");
+    },
+    getCovidByState: function(state) {
+      return axios.get("https://api.covidtracking.com/v1/states/" + state + "/current.json");
+    },
+    getCovidInfoByState: function(state) {
+      return axios.get("https://api.covidtracking.com/v1/states/" + state + "/info.json");
+    }
+  };
