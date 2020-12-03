92e5e2a4f67c8faa50553c8fc845599297a7503b
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-03 13:00:58 -0600

    Made changes to work with main branch

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home'; // import our pages
 import Flights from './pages/Flights'; // import our pages 
 import CoronaTravelNews from './pages/CoronaTravelNews'; // import our pages 
 import Blog from './pages/Blog'; // import our pages 
+import LoginSignUp from './pages/LoginSignUp'; // import our pages 
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Route exact path='/Flights' component={Flights} />
           <Route exact path='/CoronaTravelNews' component={CoronaTravelNews} />
           <Route exact path='/Blog' component={Blog} />
+          <Route exact path='/LoginSignUp' component={LoginSignUp} />
         </Switch>
       </Router>
     </div>
