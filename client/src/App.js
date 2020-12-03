012b75471dd8d21bf1ee7cf7a3d6d5276ddd85e4
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-03 12:48:52 -0600

    Added corona travel news to the app and it works with barebones

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home'; // import our pages 
 import Flights from './pages/Flights'; // import our pages 
+import CoronaTravelNews from './pages/CoronaTravelNews'; // import our pages 
+
 
 
 
@@ -13,6 +15,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/Home' component={Home} />
           <Route exact path='/Flights' component={Flights} />
+          <Route exact path='/CoronaTravelNews' component={CoronaTravelNews} />
         </Switch>
       </Router>
     </div>
---
+++ client/src/pages/CoronaTravelNews.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+import SiteNav from "../components/SiteNav";
+
+class TemplatePage extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col size="md-12">
+                            <SiteNav />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col size="md-12">
+
+                        </Col>
+                    </Row>
+                </Container >
+            </div >
+        );
+    }
+}
+export default TemplatePage;
