629f05a8bf920c13c252bbc8129267025a348b34
Charline <charlinerlach@gmail.com>
2020-12-04 11:00:11 -0600

    Added mongoose to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,12 +11,23 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/blog',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
