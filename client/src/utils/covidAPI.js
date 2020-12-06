ec8ebbbac155143fe5658b49100b9369e72dbc3e
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-05 20:21:05 -0600

    Not even sure

--- client/src/utils/covidAPI.js
+++ client/src/utils/covidAPI.js
@@ -1,15 +1,15 @@
 
-  import axios from "axios";
+import axios from "axios";
 
 
-  export default {
-    getCovidInfo: function() {
-      return axios.get("https://api.covidtracking.com/v1/states/current.json");
-    },
-    getCovidByState: function(state) {
-      return axios.get("https://api.covidtracking.com/v1/states/" + state + "/current.json");
-    },
-    getCovidInfoByState: function(state) {
-      return axios.get("https://api.covidtracking.com/v1/states/" + state + "/info.json");
-    }
-  };
+export default {
+  getCovidInfo: function () {
+    return axios.get("https://api.covidtracking.com/v1/states/current.json");
+  },
+  getCovidByState: function (state) {
+    return axios.get("https://api.covidtracking.com/v1/states/" + state + "/current.json");
+  },
+  getCovidInfoByState: function (state) {
+    return axios.get("https://api.covidtracking.com/v1/states/" + state + "/info.json");
+  }
+};
