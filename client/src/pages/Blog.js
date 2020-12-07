5dfc65ac5dcef98625d2c25c887d4f94b81c3f34
Charline <charlinerlach@gmail.com>
2020-12-07 15:02:11 -0600

    Updated page/BlogPostDetail too

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -13,55 +13,53 @@ import Footer from "../components/Footer";
 import "./style.css"
 
 
-
 function Blog() {
-// Setting our component's initial state
-const [blogs, setBlogs] = useState([])
-const [formObject, setFormObject] = useState({})
+    // Setting our component's initial state
+    const [blogs, setBlogs] = useState([])
+    const [formObject, setFormObject] = useState({})
 
-// Load all books and store them with setBooks
-useEffect(() => {
-    loadBlogs()
-}, [])
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        loadBlogs()
+    }, [])
 
-// Loads all books and sets them to books
-function loadBlogs() {
-    API.getBlogs()
-        .then(res =>
-            setBlogs(res.data)
-        )
-        .catch(err => console.log(err));
-};
+    // Loads all books and sets them to books
+    function loadBlogs() {
+        API.getBlogs()
+            .then(res =>
+                setBlogs(res.data)
+            )
+            .catch(err => console.log(err));
+    };
 
-// Deletes a book from the database with a given id, then reloads books from the db
-function deleteBlog(id) {
-    API.deleteBlog(id)
-        .then(res => loadBlogs())
-        .catch(err => console.log(err));
-}
-
-// Handles updating component state when the user types into the input field
-function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({ ...formObject, [name]: value })
-};
-
-// When the form is submitted, use the API.saveBook method to save the book data
-// Then reload books from the database
-function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.title && formObject.location && formObject.author) {
-        API.saveBlog({
-            title: formObject.title,
-            location: formObject.location,
-            author: formObject.author,
-            blogPost: formObject.synopsis
-        })
+    // Deletes a book from the database with a given id, then reloads books from the db
+    function deleteBlog(id) {
+        API.deleteBlog(id)
             .then(res => loadBlogs())
             .catch(err => console.log(err));
     }
-};
 
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({ ...formObject, [name]: value })
+    };
+
+    // When the form is submitted, use the API.saveBook method to save the book data
+    // Then reload books from the database
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formObject.title && formObject.location && formObject.author) {
+            API.saveBlog({
+                title: formObject.title,
+                location: formObject.location,
+                author: formObject.author,
+                blogPost: formObject.synopsis
+            })
+                .then(res => loadBlogs())
+                .catch(err => console.log(err));
+        }
+    };
 
     return (
         <div>
@@ -72,60 +70,60 @@ function handleFormSubmit(event) {
                     </Col>
                 </Row>
                 <Row>
-                <Col size="md-6">
-                    <Jumbotron>
-                        <h1>Travel Blog</h1>
-                    </Jumbotron>
-                    <form>
-                        <Input
-                            onChange={handleInputChange}
-                            name="title"
-                            placeholder="Title (required)"
-                        />
-                        <Input
-                            onChange={handleInputChange}
-                            name="location"
-                            placeholder="Location (required)"
-                        />
-                        <Input
-                            onChange={handleInputChange}
-                            name="author"
-                            placeholder="Author (required)"
-                        />
-                        <TextArea
-                            onChange={handleInputChange}
-                            name="blogpost"
-                            placeholder="Blog Post (optional)"
-                        />
-                        <FormBtn
-                            disabled={!(formObject.author && formObject.location)}
-                            onClick={handleFormSubmit}
-                        >
-                            Submit Post
+                    <Col size="md-6">
+                        <Jumbotron>
+                            <h1>Travel Blog</h1>
+                        </Jumbotron>
+                        <form>
+                            <Input
+                                onChange={handleInputChange}
+                                name="title"
+                                placeholder="Title (required)"
+                            />
+                            <Input
+                                onChange={handleInputChange}
+                                name="location"
+                                placeholder="Location (required)"
+                            />
+                            <Input
+                                onChange={handleInputChange}
+                                name="author"
+                                placeholder="Author (required)"
+                            />
+                            <TextArea
+                                onChange={handleInputChange}
+                                name="blogpost"
+                                placeholder="Blog Post (optional)"
+                            />
+                            <FormBtn
+                                disabled={!(formObject.author && formObject.location)}
+                                onClick={handleFormSubmit}
+                            >
+                                Submit Post
                 </FormBtn>
-                    </form>
-                </Col>
-                <Col size="md-6 sm-12">
-                    <Jumbotron>
-                        <h1>Blog Posts</h1>
-                    </Jumbotron>
-                    {blogs.length ? (
-                        <List>
-                            {blogs.map(blog => (
-                                <ListItem key={blog._id}>
-                                    <Link to={"/blogs/" + blog._id}>
-                                        <strong>
-                                            {blog.title} in {blog.location}
-                                        </strong>
-                                    </Link>
-                                    <DeleteBtn onClick={() => deleteBlog(blog._id)} />
-                                </ListItem>
-                            ))}
-                        </List>
-                    ) : (
-                            <h3>No Results to Display</h3>
-                        )}
-                </Col>
+                        </form>
+                    </Col>
+                    <Col size="md-6 sm-12">
+                        <Jumbotron>
+                            <h1>Blog Posts</h1>
+                        </Jumbotron>
+                        {blogs.length ? (
+                            <List>
+                                {blogs.map(blog => (
+                                    <ListItem key={blog._id}>
+                                        <Link to={"/blogs/" + blog._id}>
+                                            <strong>
+                                                {blog.title} in {blog.location}
+                                            </strong>
+                                        </Link>
+                                        <DeleteBtn onClick={() => deleteBlog(blog._id)} />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        ) : (
+                                <h3>No Results to Display</h3>
+                            )}
+                    </Col>
                 </Row>
                 <br></br>
                 <br></br>
@@ -139,4 +137,5 @@ function handleFormSubmit(event) {
     );
 
 };
+
 export default Blog;
