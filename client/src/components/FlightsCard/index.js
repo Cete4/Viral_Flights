37f05c40bbd3f82f481b811377ce7f8b69d29f67
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-08 11:54:54 -0600

    Getting all the little things going on

--- client/src/components/FlightsCard/index.js
+++ client/src/components/FlightsCard/index.js
@@ -10,14 +10,20 @@ function BookCard(props) {
     } else {
         str = "False";
     }
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
     return (
         <Card id="rootCard">
             {/* TODO: Add images of the destination if possible */}
             {/* <Card.Img variant="left" src={props.src} /> */}
             <Card.Body>
                 <Card.Title>Flight: {props.destination}</Card.Title>
-                <Card.Text id="heavy">Cost: {props.cost}</Card.Text>
-                <Card.Text id="heavy">Date: {props.Date}</Card.Text>
+                <Card.Text id="heavy">Cost: ${props.cost}</Card.Text>
+                <Card.Text id="heavy">Date: {today}</Card.Text>
                 <Card.Text id="heavy">Direct Flight: {str}</Card.Text>
                 <Card.Text>{props.description}</Card.Text>
             </Card.Body>
