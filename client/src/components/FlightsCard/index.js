62577b5189987383f2c03646caaafc59c02fe81d
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-08 11:52:53 -0600

    Got the flight direct working

--- client/src/components/FlightsCard/index.js
+++ client/src/components/FlightsCard/index.js
@@ -4,6 +4,12 @@ import "./style.css";
 // import flightAPI from "../utils/flightAPI";
 
 function BookCard(props) {
+    let str = "";
+    if (props.direct === true) {
+        str = "True";
+    } else {
+        str = "False";
+    }
     return (
         <Card id="rootCard">
             {/* TODO: Add images of the destination if possible */}
@@ -12,7 +18,7 @@ function BookCard(props) {
                 <Card.Title>Flight: {props.destination}</Card.Title>
                 <Card.Text id="heavy">Cost: {props.cost}</Card.Text>
                 <Card.Text id="heavy">Date: {props.Date}</Card.Text>
-                <Card.Text id="heavy">Link: <a href={props.link} target={"_blank"} rel="noopener noreferrer">{props.link}</a></Card.Text>
+                <Card.Text id="heavy">Direct Flight: {str}</Card.Text>
                 <Card.Text>{props.description}</Card.Text>
             </Card.Body>
         </Card>
