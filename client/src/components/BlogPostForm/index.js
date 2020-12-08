d36c6573091a573beab46e19df5ccde163b2e480
Charline <charlinerlach@gmail.com>
2020-12-08 11:03:31 -0600

    removed home page error so I could successfully push to Heroku

--- client/src/components/BlogPostForm/index.js
+++ client/src/components/BlogPostForm/index.js
@@ -1,124 +1,124 @@
-import React, { useState, useEffect } from "react";
-import DeleteBtn from "../DeleteBtn";
-import Jumbotron from "../Jumbotron";
-import API from "../../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../Grid";
-import { List, ListItem } from "../List";
-import { Input, TextArea, FormBtn } from "../Form";
+// import React, { useState, useEffect } from "react";
+// import DeleteBtn from "../DeleteBtn";
+// import Jumbotron from "../Jumbotron";
+// import API from "../../utils/API";
+// import { Link } from "react-router-dom";
+// import { Col, Row, Container } from "../Grid";
+// import { List, ListItem } from "../List";
+// import { Input, TextArea, FormBtn } from "../Form";
 
-function BlogPostForm() {
-  // Setting our component's initial state
-  const [blogs, setBlogs] = useState([])
-  const [formObject, setFormObject] = useState({})
+// function BlogPostForm() {
+//   // Setting our component's initial state
+//   const [blogs, setBlogs] = useState([])
+//   const [formObject, setFormObject] = useState({})
 
-  // Load all books and store them with setBooks
-  useEffect(() => {
-    loadBlogs()
-  }, [])
+//   // Load all books and store them with setBooks
+//   useEffect(() => {
+//     loadBlogs()
+//   }, [])
 
-  // Loads all books and sets them to books
-  function loadBlogs() {
-    API.getBlogs()
-      .then(res =>
-        setBlogs(res.data)
-      )
-      .catch(err => console.log(err));
-  };
+//   // Loads all books and sets them to books
+//   function loadBlogs() {
+//     API.getBlogs()
+//       .then(res =>
+//         setBlogs(res.data)
+//       )
+//       .catch(err => console.log(err));
+//   };
 
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBlog(id) {
-    API.deleteBlog(id)
-      .then(res => loadBlogs())
-      .catch(err => console.log(err));
-  }
+//   // Deletes a book from the database with a given id, then reloads books from the db
+//   function deleteBlog(id) {
+//     API.deleteBlog(id)
+//       .then(res => loadBlogs())
+//       .catch(err => console.log(err));
+//   }
 
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({ ...formObject, [name]: value })
-  };
+//   // Handles updating component state when the user types into the input field
+//   function handleInputChange(event) {
+//     const { name, value } = event.target;
+//     setFormObject({ ...formObject, [name]: value })
+//   };
 
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.title && formObject.location && formObject.author) {
-      API.saveBlog({
-        title: formObject.title,
-        location: formObject.location,
-        author: formObject.author,
-        blogPost: formObject.synopsis
-      })
-        .then(res => loadBlogs())
-        .catch(err => console.log(err));
-    }
-  };
+//   // When the form is submitted, use the API.saveBook method to save the book data
+//   // Then reload books from the database
+//   function handleFormSubmit(event) {
+//     event.preventDefault();
+//     if (formObject.title && formObject.location && formObject.author) {
+//       API.saveBlog({
+//         title: formObject.title,
+//         location: formObject.location,
+//         author: formObject.author,
+//         blogPost: formObject.synopsis
+//       })
+//         .then(res => loadBlogs())
+//         .catch(err => console.log(err));
+//     }
+//   };
 
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="md-6">
-          <Jumbotron>
-            <h1>Past Travel Blog!</h1>
-          </Jumbotron>
-          <form>
-            <Input
-              onChange={handleInputChange}
-              name="title"
-              placeholder="Title (required)"
-            />
-            <Input
-              onChange={handleInputChange}
-              name="location"
-              placeholder="Location (required)"
-            />
-            <Input
-              onChange={handleInputChange}
-              name="author"
-              placeholder="Blogger (required)"
-            />
-            <TextArea
-              onChange={handleInputChange}
-              name="blogPost"
-              placeholder="Blog Post (optional)"
-            />
-            <FormBtn
-              disabled={!(formObject.author && formObject.location)}
-              onClick={handleFormSubmit}
-            >
-              Submit Post
-              </FormBtn>
-          </form>
-        </Col>
-        <Col size="md-6 sm-12">
-          <Jumbotron>
-            <h1>Blog Post List</h1>
-          </Jumbotron>
-          {blogs.length ? (
-            <List>
-              {blogs.map(blog => (
-                <ListItem key={blog._id}>
-                  <Link to={"/blogs/" + blog._id}>
-                    <strong>
-                      {blog.title} in {blog.location}
-                    </strong>
-                  </Link>
-                  <DeleteBtn onClick={() => deleteBlog(blog._id)} />
-                </ListItem>
-              ))}
-            </List>
-          ) : (
-              <div class="card bg-light">
-                <div class="card-body">
-                  <h3>No Results to Display</h3>
-                </div>
-              </div>
-            )}
-        </Col>
-      </Row>
-    </Container>
-  );
-}
+//   return (
+//     <Container fluid>
+//       <Row>
+//         <Col size="md-6">
+//           <Jumbotron>
+//             <h1>Past Travel Blog!</h1>
+//           </Jumbotron>
+//           <form>
+//             <Input
+//               onChange={handleInputChange}
+//               name="title"
+//               placeholder="Title (required)"
+//             />
+//             <Input
+//               onChange={handleInputChange}
+//               name="location"
+//               placeholder="Location (required)"
+//             />
+//             <Input
+//               onChange={handleInputChange}
+//               name="author"
+//               placeholder="Blogger (required)"
+//             />
+//             <TextArea
+//               onChange={handleInputChange}
+//               name="blogPost"
+//               placeholder="Blog Post (optional)"
+//             />
+//             <FormBtn
+//               disabled={!(formObject.author && formObject.location)}
+//               onClick={handleFormSubmit}
+//             >
+//               Submit Post
+//               </FormBtn>
+//           </form>
+//         </Col>
+//         <Col size="md-6 sm-12">
+//           <Jumbotron>
+//             <h1>Blog Post List</h1>
+//           </Jumbotron>
+//           {blogs.length ? (
+//             <List>
+//               {blogs.map(blog => (
+//                 <ListItem key={blog._id}>
+//                   <Link to={"/blogs/" + blog._id}>
+//                     <strong>
+//                       {blog.title} in {blog.location}
+//                     </strong>
+//                   </Link>
+//                   <DeleteBtn onClick={() => deleteBlog(blog._id)} />
+//                 </ListItem>
+//               ))}
+//             </List>
+//           ) : (
+//               <div class="card bg-light">
+//                 <div class="card-body">
+//                   <h3>No Results to Display</h3>
+//                 </div>
+//               </div>
+//             )}
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
 
-export default BlogPostForm;
+// export default BlogPostForm;
--- client/src/pages/BlogPostDetail.js
+++ client/src/pages/BlogPostDetail.js
@@ -12,11 +12,11 @@ import API from "../utils/API";
 function BlogPostDetail() {
     const [blog, setBlog] = useState({})
 
-    const {id} = useParams()
+    const { id } = useParams()
     useEffect(() => {
-      API.getBlog(id)
-        .then(res => setBlog(res.data))
-        .catch(err => console.log(err));
+        API.getBlog(id)
+            .then(res => setBlog(res.data))
+            .catch(err => console.log(err));
     }, [])
 
     return (
@@ -30,25 +30,28 @@ function BlogPostDetail() {
                 <Row>
                     <Col size="md-12">
                         <Jumbotron>
-                            <h1>
-                                {blog.title} in {blog.location} by {blog.author}
-                            </h1>
+                            <h1>{blog.title} in {blog.location}</h1>
+                            <h2>Written by {blog.author}</h2>
                         </Jumbotron>
                     </Col>
                 </Row>
                 <Row>
-                    <Col size="md-10 md-offset-1">
+                    <Col size="md-12" style="backgroundColor:white">
                         <article>
-                            <h1>Blog Post</h1>
-                            <p>
-                                {blog.blogPost}
-                            </p>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h1>Blog Post</h1>
+                                    <p>
+                                        {blog.blogPost}
+                                    </p>
+                                </div>
+                            </div>
                         </article>
                     </Col>
                 </Row>
                 <Row>
-                    <Col size="md-2">
-                        <Link to="/Blog">← Back to Posts</Link>
+                    <Col size="md-3">
+                        <a class="btn btn-light" href="/Blog" role="button">← Back to Posts</a>
                     </Col>
                 </Row>
                 <br></br>
