7e21a418ea528ee887964aafedb997800214585b
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-09 10:01:19 -0600

    Added some changes

--- client/src/pages/Flights.js
+++ client/src/pages/Flights.js
@@ -5,12 +5,16 @@ import SiteNav from "../components/SiteNav";
 import FlightsCard from "../components/FlightsCard";
 import Footer from "../components/Footer";
 import "./style.css"
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+
 function Home() {
     // Setting our component's initial state
     const [flights, setFlights] = useState([])
     //Defaults for the time being
-    const origin = "MSP";
-    const destination = "SFO";
+    let origin = "MSP";
+    let destination = "SFO";
 
     // Load all states and store them with setStates
     useEffect(() => {
@@ -21,7 +25,6 @@ function Home() {
             }).catch((error) => {
                 console.log(error)
             })
-
         }
         f();
     }, [])
@@ -37,6 +40,97 @@ function Home() {
                 <Row>
                     <Col size="auto">
                         <h1 id="hotflights">Hot Flights</h1>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col size="6">
+                        <DropdownButton id="dropdown-item-button" title="Select State">
+                            <Dropdown.Item as="button" value="BHM" onclick="">Birmingham</Dropdown.Item>
+                            <Dropdown.Item as="button" value="ANC">Anchorage</Dropdown.Item>
+                            <Dropdown.Item as="button" value="JNU" onclick="">Juneau</Dropdown.Item>
+                            <Dropdown.Item as="button" value="PHX">Phoenix</Dropdown.Item>
+                            <Dropdown.Item as="button" value="LIT">Little Rock</Dropdown.Item>
+                            <Dropdown.Item as="button" value="LAX">Los Angeles</Dropdown.Item>
+                            <Dropdown.Item as="button" value="COS">Colorado Springs</Dropdown.Item>
+                            <Dropdown.Item as="button" value="HVN">New Haven</Dropdown.Item>
+                            <Dropdown.Item as="button" value="FLL">Fort Lauderdale</Dropdown.Item>
+                            <Dropdown.Item as="button" value="GNV">Gainesville</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Atlanta</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Honolulu</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Wichita</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Cincinnati</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">New Orleans</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bangor</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Boston</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Detroit</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bemidji</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Brainerd</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Duluth</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Minneapolis</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bozeman</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Las Vegas</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Atlantic City</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Albuquerque</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">New York</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Charlotte</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bismarck</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Columbus</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Cincinnati</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Portland</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Pittsburgh</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Philadelphia</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Charleston</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Aberdeen</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Nashville</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Dallas</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Salt Lake City</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Washington, D.C.</Dropdown.Item>
+                        </DropdownButton>
+                    </Col>
+                    <Col size="6">
+                        <DropdownButton id="dropdown-item-button" title="Select State">
+                            <Dropdown.Item as="button" value="BHM" onclick="">Birmingham</Dropdown.Item>
+                            <Dropdown.Item as="button" value="ANC">Anchorage</Dropdown.Item>
+                            <Dropdown.Item as="button" value="JNU" onclick="">Juneau</Dropdown.Item>
+                            <Dropdown.Item as="button" value="PHX">Phoenix</Dropdown.Item>
+                            <Dropdown.Item as="button" value="LIT">Little Rock</Dropdown.Item>
+                            <Dropdown.Item as="button" value="LAX">Los Angeles</Dropdown.Item>
+                            <Dropdown.Item as="button" value="COS">Colorado Springs</Dropdown.Item>
+                            <Dropdown.Item as="button" value="HVN">New Haven</Dropdown.Item>
+                            <Dropdown.Item as="button" value="FLL">Fort Lauderdale</Dropdown.Item>
+                            <Dropdown.Item as="button" value="GNV">Gainesville</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Atlanta</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Honolulu</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Wichita</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Cincinnati</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">New Orleans</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bangor</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Boston</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Detroit</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bemidji</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Brainerd</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Duluth</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Minneapolis</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bozeman</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Las Vegas</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Atlantic City</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Albuquerque</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">New York</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Charlotte</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Bismarck</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Columbus</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Cincinnati</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Portland</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Pittsburgh</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Philadelphia</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Charleston</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Aberdeen</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Nashville</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Dallas</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Salt Lake City</Dropdown.Item>
+                            <Dropdown.Item as="button" value="">Washington, D.C.</Dropdown.Item>
+                        </DropdownButton>
                     </Col>
                 </Row>
                 {<Row>
